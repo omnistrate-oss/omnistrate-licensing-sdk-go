d54946d52b6518e1e3bb282329e32e103a377acb
pberton <1789738+pberton@users.noreply.github.com>
2025-12-02 14:06:22 -0800

    Improve validation for intermediate certs (#33)
    Lets encrypt retired R10 and R11 before expiration. R12 and R13 were
    introduced.
    We should not pin point the intermediate certificates. Intermediate
    certificates are included on the secret.
    As fallback, updating to new certificates to allow it to succeed even if
    the intermediate is nor present.

--- pkg/certificate/certificate.go
+++ pkg/certificate/certificate.go
@@ -29,6 +29,16 @@ var intermediatePEM10 []byte
 //go:embed r11.pem
 var intermediatePEM11 []byte
 
+// Intermediate cert for RSA from https://letsencrypt.org/certificates/
+//
+//go:embed r12.pem
+var intermediatePEM12 []byte
+
+// Intermediate cert for RSA from https://letsencrypt.org/certificates/
+//
+//go:embed r13.pem
+var intermediatePEM13 []byte
+
 func LoadCertificate(certPath string) (*x509.Certificate, error) {
 	data, err := os.ReadFile(certPath)
 	if err != nil {
@@ -43,6 +53,38 @@ func LoadCertificateFromBytes(data []byte) (*x509.Certificate, error) {
 		return nil, fmt.Errorf("failed to parse certificate PEM")
 	}
 	return x509.ParseCertificate(cert)
+}
+
+func LoadCertificateChain(certPath string) ([]*x509.Certificate, error) {
+	data, err := os.ReadFile(certPath)
+	if err != nil {
+		return nil, err
+	}
+	return LoadCertificateChainFromBytes(data)
+}
+
+func LoadCertificateChainFromBytes(data []byte) ([]*x509.Certificate, error) {
+	var certs []*x509.Certificate
+	rest := data
+	for {
+		var block *pem.Block
+		block, rest = pem.Decode(rest)
+		if block == nil {
+			break
+		}
+		if block.Type != "CERTIFICATE" || len(block.Headers) != 0 {
+			continue
+		}
+		cert, err := x509.ParseCertificate(block.Bytes)
+		if err != nil {
+			return nil, err
+		}
+		certs = append(certs, cert)
+	}
+	if len(certs) == 0 {
+		return nil, fmt.Errorf("no certificates found in PEM data")
+	}
+	return certs, nil
 }
 
 func LoadPrivateKey(keyPath string) (*rsa.PrivateKey, error) {
@@ -92,6 +134,10 @@ func hash(data []byte) []byte {
 }
 
 func VerifyCertificate(cert *x509.Certificate, dnsName string, currentTime time.Time) error {
+	return VerifyCertificateWithIntermediates(cert, dnsName, currentTime, nil)
+}
+
+func VerifyCertificateWithIntermediates(cert *x509.Certificate, dnsName string, currentTime time.Time, intermediates []*x509.Certificate) error {
 	// Create a certificate pool with CA and intermediate certificates
 	rootsPool := x509.NewCertPool()
 	rootsPool.AppendCertsFromPEM(rootPEM)
@@ -99,7 +145,14 @@ func VerifyCertificate(cert *x509.Certificate, dnsName string, currentTime time.
 	intermediatesPool := x509.NewCertPool()
 	intermediatesPool.AppendCertsFromPEM(intermediatePEM10)
 	intermediatesPool.AppendCertsFromPEM(intermediatePEM11)
+	intermediatesPool.AppendCertsFromPEM(intermediatePEM12)
+	intermediatesPool.AppendCertsFromPEM(intermediatePEM13)
 
+	for _, intermediate := range intermediates {
+		intermediatesPool.AddCert(intermediate)
+	}
+
+	// Verify the certificate
 	_, err := cert.Verify(x509.VerifyOptions{
 		DNSName:       dnsName,
 		CurrentTime:   currentTime,
